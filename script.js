cedf59a820b9dfbe3bc65d0f3f004c79df713f20
code_unlimited <137721178+Hossain86@users.noreply.github.com>
2024-03-08 20:38:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const defaultQuotes2=[
+  "I'm not the only one who doesn't know what to do.",
+  'The future belongs to those who believe in the beauty of their dreams.',
+  `It is often said, "The pen is mightier than the sword." And it's true words can hold a lot of power.`,
+  `"It is during our darkest moments that we must focus to see the light." — Aristotle`,
+  `Time is a created thing. To say 'I don't have time is like saying I don't want to.`,
+  `"I am lucky that whatever fear I have inside me, my desire to win is always stronger." — Serena Williams`,
+  `"Just don't give up trying to do what you really want to do. Where there is love and inspiration, I don't think you can go wrong." — Ella Fitzgerald`,
+  `"If you risk nothing, then you risk everything." — Geena Davis`,
+  `"Failure is the condiment that gives success its flavor." — Truman Capote`,
+  `"When it comes to luck, you make your own." — Bruce Springsteen`,
+  `"No matter how far away from yourself you may have strayed, there is always a path back. You already know who you are and how to fulfill your destiny." — Oprah Winfrey`,
+  `"A problem is a chance for you to do your best." — Duke Ellington`,
+  `“We cannot solve problems with the kind of thinking we employed when we came up with them.” —Albert Einstein`,
+  `“The road to success and the road to failure are almost exactly the same.” —Colin R. Davis`,
+  `“Success usually comes to those who are too busy to be looking for it.” —Henry David Thoreau`,
+  `“There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.” —Mister Rogers`,
+  `“I never dreamed about success. I worked for it.” —Estée Lauder`,
+  `“Experience is a hard teacher because she gives the test first, the lesson afterward.” ―Vernon Sanders Law`,
+  `“You learn more from failure than from success. Don’t let it stop you. Failure builds character.” —Unknown`,
+  `“Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all
+   matters of the heart, you’ll know when you find it.” —Steve Jobs`,
+  `“A man who has committed a mistake and doesn’t correct it is committing another mistake.” —Confucius Kongzi`,
+  `“A successful man is one who can lay a firm foundation with the bricks others have thrown at him.” —David Brinkley`,
+  `“He is a wise man who does not grieve for the things which he has not, but rejoices for those which he has.” —Epictetus`,
+  `“Take the attitude of a student, never be too big to ask questions, never know too much to learn something new.” —Augustine Og Mandino`,
+];
+const defaultQuotes = [
+  `ওরা তোমাকে নিয়ে ঠাট্টা করুক , তোমাকে নিয়ে হাসুক , তোমাকে আঘাত করুক , অবজ্ঞা করুক তাতে কিছুই হবে না । কিন্তু তারা যেন তোমাকে থামাতে না পারে।`,
+  `যে ব্যাপারে তুমি সত্যিই বিশ্বাস করো , সে ব্যাপারে কখনো হাল ছেড়ো না , পথ তুমি খুঁজে পাবেই ।`,
+  ` সহজে জেতার আনন্দ কোথায় ? বাধা যত বিশাল , বিজয়ের আনন্দও ততই বাঁধভাঙ্গা ।`,
+  `কখনো হাল ছেড়ে দিও না । এখনকার এই দাঁতে দাঁত চেপে করা কষ্টগুলো তোমাকে বিজয়ীর খেতাব দেবে সারাজীবনের জন্য ।`,
+  ` তোমার জীবনে তুমি যা নিয়েই কাজ করো , তাকে সবচেয়ে ভালো ভাবে করার চেষ্টা করো । এমন ভাবে করো , যেন তোমার আগে পরে কেউ এতটা ভালো করে করতে না পারে ।`,
+  `আমি ব্যর্থতাকে মেনে নিতে পারি , কিন্তু আমি চেষ্টা না করাকে মেনে নিতে পারি না।`,
+  ` কখনো ভেঙে পড়ো না । পৃথিবীতে যা কিছু হারিয়ে যায় , অন্য কোন রূপে সেটি ঠিকই আবার ফিরে আসে জীবনে।`,
+  `তোমার স্বপ্ন আর তোমার মাঝে দাঁড়িয়ে আছে কেবল একটি জিনিস , সেটি হচ্ছে অজুহাত ! যে মুহূর্ত থেকে তুমি নিজেকে অজুহাত দেখানো বন্ধ করে কাজ শুরু করবে , সে মুহূর্ত থেকে তোমার স্বপ্ন আর স্বপ্ন থাকবে না —- সেটি বাস্তবে রূপ নিতে শুরু করবে ।`,
+  `একটি লক্ষ্য ঠিক করো । সেই লক্ষ্যকে নিজের জীবনের অংশ বানিয়ে ফেলো । চিন্তা করো , স্বপ্ন দেখো , তোমার মস্তিষ্ক , পেশী , রক্তনালী পুরো শরীরে সেই লক্ষ্যকে ছড়িয়ে দাও , আর বাকি সবকিছু ভুলে যাও । —–এটাই সাফল্যের পথ ।`,
+  `সাফল্যের মূল মন্ত্র হল যা আমরা ভয় পাই তার উপর নয় বরং আমরা যা চাই তার উপর আমাদের চেতন মনকে কেন্দ্রীভূত করা ।`,
+  `আপনি যা করছেন , যদি ভালোবাসেন এবং ওই কাজে সফলতার জন্য সবকিছু করতে ইচ্ছুক থাকেন , তাহলে সেটা হাতের নাগালে পৌঁছোবে । কাজের পেছনে প্রতিটা মিনিটের মূল্যায়ন প্রয়োজন । ভাবুন আর ভাবুন ; আসলেই আপনি কি নকশা অথবা তৈরি করতে চাচ্ছেন ।`,
+  ` আকাশের দিকে তাকাও । আমরা একা নই । পুরো মহাবিশ্ব আমাদের প্রতি বন্ধুত্বসুলভ । যারা স্বপ্ন দেখে এবং কাজ করে শুধুমাত্র তাদেরকেই শ্রেষ্ঠটা দেওয়ার জন্য চক্রান্তে লিপ্ত এই বিশ্ব ।`,
+  ` প্রত্যেকের জীবনের একটা গল্প আছে । অতীতে ফিরে গিয়ে গল্পের শুরুটা কখনো পরিবর্তন করা সম্ভব নয় , কিন্তু কঠোর পরিশ্রমের মাধ্যমে গল্পের শেষটা চাইলেই নতুন করে সাজিয়ে তুলতে পারো ।`,
+  `ব্যর্থতা মানে হচ্ছে , ব্যর্থতা — কেন হয়েছে , কার কারণে হয়েছে সেগুলো কেউ জানতে চাইবে না । তুমি ব্যর্থ হলে তার যন্ত্রণা তোমাকে একদম একাকী সইতে হবে , কেউ তোমার পাশে এসে দাঁড়াবে না । তাই কখনো অজুহাত বানাবে না , অন্যদের সুযোগ দেবে না —তোমার জীবনটাকে নিয়ন্ত্রণ করার । জিততে তোমাকে হবেই ।`,
+  `জীবনে অনেক বিষয় আছে যেগুলো তোমার নিয়ন্ত্রণের বাইরে এবং সেগুলো নিয়ে মাথা ঘামানোরও মানে হয় না , কারণ এর বাইরে ও তোমার হাতে হাজার হাজার জিনিস রয়েছে যেগুলো তুমি বিজয় করতে পারো ।`,
+  `ব্যর্থতাকে ভয় করার বদলে চেষ্টা না করে বসে থাকাকে ভয় করো ।`,
+  `হার মেনো না । আজকের দিনটা কঠিন , কাল হবে অন্ধকার , কিন্তু তারপর সূর্যকে উঠতেই হবে ।`,
+  `তুমি যদি এখন থেকেই তোমার স্বপ্ন গুলো সত্যি করার পেছনে ছুটে না চলো , একদিন তোমাকে কাজ করতে হবে অন্যদের জীবনে তাদের স্বপ্ন গুলো সত্যি করার জন্য ।`,
+  `সফল হতে চাইলে তোমার সামনে আসা সব চ্যালেঞ্জ মোকাবিলা করতে হবে । চ্যালেঞ্জ বেছে নেওয়ার কোনও সুযোগ নেই ।`,
+  `কখনও না পড়ে যাওয়ার মাঝে বীরত্ব নেই , পড়ে গিয়ে উঠে দাঁড়ানোর মাঝেই সত্যিকারের বীরত্ব লুকিয়ে আছে ।`,
+  `তুমি কত ধীরে চলেছ , সেটা কোনও ব্যাপার নয় ; না থেমে চলতে থাকাটাই আসল কথা ।`,
+  `কোন কিছু যদি সত্যিই তোমার কাছে গুরুত্বপূর্ণ হয় , কোন বাধাই তোমাকে থামাতে পারবে না ।`,
+  `যদি তোমার লক্ষ্য নিয়ে কেউ হাসি তামাশা না করে , তবে বুঝতে হবে তোমার লক্ষ্যটি খুব ছোট ।`,
+  `আমি স্বপ্ন দেখেছিলাম , সেই স্বপ্নে আস্থা ছিল । আর আমি কাজটা ভালোবাসতাম । ফেসবুক বিফল হলেও আমার ভালোবাসাটা থাকত । জীবনে একটা স্বপ্ন থাকতে হয় সেই স্বপ্নকে ভালোও বসতে হয়।`,
+  `স্বপ্ন দেখতে জানলে জীবনের কাঁটাগুলোও ধরা দেয় গোলাপ হয়ে ।`,
+  `জীবনে সবকিছু একবার হলেও চেষ্টা করে দেখা উচিত । স্রষ্টা প্রতিটি মানুষকে কিছু না কিছু অনুপম দক্ষতা দিয়ে পাঠিয়েছেন , তুমি সেটিকে কখনো জানতেও পারবে না , যতদিন না তুমি সেটা চেষ্টা করে দেখছো।`,
+  `সত্যিকারের জ্ঞানী মানুষ আমরা তখনই হতে পারি যখন বুঝতে পারি , আমরা আমাদের সম্পর্কে , জীবন সম্পর্কে এবং আমাদের চারপাশের জগত সম্পর্কে কতটা কম জানি ।`,
+  `তুমি তোমার মতো হও , সবাই যে যার মত হয়ে গেছে ।`,
+  `প্রকৃতির গোপন শক্তিটিকে অর্জন করার চেষ্টা করো । গোপন শক্তিটি হল ধৈর্য ।`,
+  `মানুষের পক্ষে সব স্বপ্নই পূরণ করা সম্ভব যদি সে যথেষ্ট সাহসী হয় ।`,
+];
+
+window.onload = function(){
+  main();
+}
+
+function main(){
+  const newQuoteBtn= document.getElementById('new-quote-btn');
+  const quoteBody=document.getElementById('quote-body');
+
+  newQuoteBtn.addEventListener('click',()=>{
+      const randomIndex=Math.floor(Math.random()*defaultQuotes.length);
+      quoteBody.innerHTML=defaultQuotes[randomIndex];
+    })
+}
